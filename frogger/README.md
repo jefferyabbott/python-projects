568ceab7209cb6015bc9d4e1c2b73fd74f459c97
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-14 12:25:00 -0400

    Add Frogger

---
+++ frogger/README.md
@@ -0,0 +1,5 @@
+## Frogger
+
+Clone of the classic Frogger game, but using a turtle instead of a frog.
+
+![Frogger](https://www.dropbox.com/scl/fi/9x4bm7hsisvf8925l2o96/frogger.png?rlkey=4tszeji21kzt6em0xcn6wjvc2&st=hwikoasr&raw=1)
---
+++ frogger/main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player, FINISH_LINE_Y
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    screen.onkey(player.go_up, "Up")
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # detect when the player reaches the other side
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
