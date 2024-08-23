fb0f15739b2f21d7a1b0f4be3f49545b2b3aae3f
Jeffery Abbott <jeff@jefferyabbott.com>
2024-08-23 07:44:46 -0400

    Add search feature and convert data to JSON

--- password manager/main.py
+++ password manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from password_generator import generate_random_password
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -12,11 +13,33 @@ def generate_password():
     pyperclip.copy(generated_password)
 
 
+# --------------------------- SEARCH PASSWORD ------------------------------ #
+def search_passwords():
+    search_term = website_entry.get()
+    if search_term == '':
+        messagebox.showinfo(title="Missing website", message="Please enter a website.")
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            message = f"Website: {search_term}\nUsername/Email: {data[search_term]['email']}\nPassword: {data[search_term]['password']}"
+            messagebox.showinfo(title=f"{search_term} details", message=message)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Missing data file", message="There is no data to search.")
+    except KeyError as error_message:
+        messagebox.showinfo(title="Website not found", message="This website was not found in the data file.")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if website == '' or email == '' or password == '':
         messagebox.showinfo(title="Missing info!", message="Please enter all required information.")
@@ -28,16 +51,23 @@ def save_password():
     if not is_ok:
         return
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-
-    website_entry.delete(0, END)
-    email_entry.delete(0, END)
-    password_entry.delete(0, END)
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    else:
+        data.update(new_data)
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+    finally:
+        website_entry.delete(0, END)
+        email_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -50,9 +80,12 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=38)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
+
+search_button = Button(text="Search", width=13, command=search_passwords)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
