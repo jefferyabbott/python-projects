b842f65df804a1cd83a9189d705555ab85c6eaa6
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-05 18:11:10 -0400

    remove search for IATA codes

--- flight deals/main.py
+++ flight deals/main.py
@@ -8,11 +8,11 @@ flight_search = FlightSearch()
 sheet_data = google_sheet.get_sheet()
 
 # lookup IATA codes
-for city in sheet_data:
-    if city['iataCode'] == '':
-        city['iataCode'] = flight_search.lookup_iata_code(city['city'])
-        google_sheet.update_iata_code(city['id'], city['iataCode'])
-        time.sleep(2)
+# for city in sheet_data:
+#     if city['iataCode'] == '':
+#         city['iataCode'] = flight_search.lookup_iata_code(city['city'])
+#         google_sheet.update_iata_code(city['id'], city['iataCode'])
+#         time.sleep(2)
 
-sheet_data = google_sheet.get_sheet()
+# sheet_data = google_sheet.get_sheet()
 print(sheet_data)
