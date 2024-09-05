bcd3fb914024d7b713bc75d0b670facce7e47be8
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-05 18:08:58 -0400

    get IATA codes

--- flight deals/flight_search.py
+++ flight deals/flight_search.py
@@ -3,15 +3,47 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+
 class FlightSearch:
     def __init__(self):
-        self.headers = {
-            "Authorization": f"Bearer ${os.getenv('AMADEUS_API_KEY')}"
+        self.token = self._get_new_token()
+
+
+    def _get_new_token(self):
+        request_token_url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
-        self.base_url = "https://test.api.amadeus.com/v1/reference-data/locations"
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': os.getenv('AMADEUS_API_KEY'),
+            'client_secret': os.getenv('AMADEUS_API_SECRET'),
+        }
+        response = requests.post(url=request_token_url, headers=header, data=body)
+        response.raise_for_status()
+        return response.json()['access_token']
 
 
     def lookup_iata_code(self, city):
-        print("lookup ", city)
-        return "TESTING"
+        print(f"Getting IATA code for {city}")
+        headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        query = {
+            "keyword": city,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(url=IATA_ENDPOINT, headers=headers, params=query)
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city}.")
+            return "Not Found"
+
+        return code
 
--- flight deals/main.py
+++ flight deals/main.py
@@ -1,5 +1,6 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+import time
 
 google_sheet = DataManager()
 flight_search = FlightSearch()
@@ -10,8 +11,8 @@ sheet_data = google_sheet.get_sheet()
 for city in sheet_data:
     if city['iataCode'] == '':
         city['iataCode'] = flight_search.lookup_iata_code(city['city'])
-        print(city)
         google_sheet.update_iata_code(city['id'], city['iataCode'])
+        time.sleep(2)
 
 sheet_data = google_sheet.get_sheet()
 print(sheet_data)
