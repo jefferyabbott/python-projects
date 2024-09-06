bcdca07062c83ce41653f8f2e72a3e9b0cc9ab19
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-06 04:21:21 -0400

    Implement search for cheapest flights

--- flight deals/main.py
+++ flight deals/main.py
@@ -1,18 +1,37 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
 import time
+from datetime import datetime, timedelta
 
-google_sheet = DataManager()
+data_manager = DataManager()
 flight_search = FlightSearch()
+sheet_data = data_manager.get_sheet()
 
-sheet_data = google_sheet.get_sheet()
+ORIGIN_CITY = "LON"
 
-# lookup IATA codes
-# for city in sheet_data:
-#     if city['iataCode'] == '':
-#         city['iataCode'] = flight_search.lookup_iata_code(city['city'])
-#         google_sheet.update_iata_code(city['id'], city['iataCode'])
-#         time.sleep(2)
+# update airport codes in spreadsheet
+for city in sheet_data:
+    if city['iataCode'] == '':
+        city['iataCode'] = flight_search.lookup_iata_code(city['city'])
+        data_manager.update_iata_code(city['id'], city['iataCode'])
+        time.sleep(2)
 
-# sheet_data = google_sheet.get_sheet()
-print(sheet_data)
+# search for flights
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}.")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY,
+        destination['iataCode'],
+        from_time = tomorrow,
+        to_time = six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: {cheapest_flight.price}")
+    time.sleep(2)
+
+
+
