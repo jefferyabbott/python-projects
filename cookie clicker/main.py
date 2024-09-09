524347e5f72bcac76c4d5a5827de5a182bfebd92
Jeffery Abbott <jeff@jefferyabbott.com>
2024-09-09 08:34:12 -0400

    Fix clicker to wait until there are more cookies to purchase stuff

--- cookie clicker/main.py
+++ cookie clicker/main.py
@@ -11,22 +11,25 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(URL)
 
 # sleep while page loads
-time.sleep(10)
+time.sleep(5)
 
 # select the language (English)
 language_button_eng = driver.find_element(By.XPATH, value='//*[@id="langSelect-EN"]')
 language_button_eng.click()
 
 # sleep while page loads
-time.sleep(10)
+time.sleep(5)
 
 cookie = driver.find_element(By.XPATH, value='//*[@id="bigCookie"]')
 available_addons = driver.find_elements(By.CLASS_NAME, value='enabled')
+last_purchased_index = -1
 
 while True:
-    # click button for 5 seconds
+    # click button for 5 seconds and then check what's available to purchase
     start_time = time.time()
     while time.time() - start_time < 5:
         cookie.click()
     available_addons = driver.find_elements(By.CLASS_NAME, value='enabled')
-    available_addons[len(available_addons) - 1].click()
+    if len(available_addons) -1 > last_purchased_index:
+        available_addons[len(available_addons) - 1].click()
+        last_purchased_index = len(available_addons) - 1
